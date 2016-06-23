4aa36b66c12f3b34011b8f5c37ab11b72fab14aa
Tibor Kiss <tiborkiss1008@gmail.com>
2016-06-23 09:16:07 +0200

    Paper garbage logic ready

--- paper_garbage.py
+++ paper_garbage.py
@@ -3,5 +3,8 @@ from garbage import Garbage
 
 class PaperGarbage(Garbage):
 
-    def __init__(self, is_squeezed):
+    def __init__(self, is_squeezed=None):
         self.is_squeezed = is_squeezed
+
+    def squeeze(self):
+        self.is_squeezed = True
